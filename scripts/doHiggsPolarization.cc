e7d4110d25f6a70961d9a8bfd4de7afa90256a8f
BlancoFS <60780075+BlancoFS@users.noreply.github.com>
2022-12-23 12:04:00 +0100

    Add files via upload

--- scripts/doHiggsPolarization.cc
+++ scripts/doHiggsPolarization.cc
@@ -33,6 +33,136 @@
 #include "TMath.h"
 
 
+Double_t MW = 80.3579736098775;
+Double_t WW = 2.084298998278219;
+Double_t s2w = 0.22283820939806098;
+Double_t c2w = 0.777161790601939;
+Double_t cL= 4.0*sqrt(c2w/2.0);
+Double_t cR = 0.0;
+
+Double_t GF = ROOT::Math::sqrt(2)/(2*246*246);
+Double_t gW = ROOT::Math::sqrt((GF/ROOT::Math::sqrt(2)) * 8 * MW*MW);
+Double_t ghWW = gW*MW;
+
+TVector3 rotinv(TVector3 p, TVector3 q){
+
+  TVector3 pp(0.0, 0.0, 0.0);
+
+  double qmodt = q.X()*q.X()+q.Y()*q.Y();
+  double qmod = qmodt + q.Z()*q.Z();
+  qmodt = TMath::Sqrt(qmodt);
+  qmod = TMath::Sqrt(qmod);
+  
+  if (qmod==0.0){
+    cout << "Error while rotation" << endl;
+    return pp;
+  }
+
+  double cth = q.Z() / qmod;
+  double sth = 1.0 - cth*cth;
+
+  if (sth==0.0){
+    pp = p;
+    return pp;
+  }
+
+  sth = TMath::Sqrt(sth);
+  
+  if (qmodt==0.0){
+    pp = p;
+    return pp;
+  }
+
+  double cfi = q.X() / qmodt;
+  double sfi = q.Y() / qmodt;
+
+  double p1 = p.X();
+  double p2 = p.Y();
+  double p3 = p.Z();
+
+  double pp1 = cth*cfi*p1+cth*sfi*p2-sth*p3;
+  double pp2 = -sfi*p1+cfi*p2;
+  double pp3 = sth*cfi*p1+sth*sfi*p2+cth*p3;
+  
+  pp.SetXYZ(pp1, pp2, pp3);
+  return pp;
+}
+
+
+TLorentzVector boostinv(TLorentzVector q, TLorentzVector pboost){
+
+  TLorentzVector qprime(0.0, 0.0, 0.0, 0.0); 
+
+  double rmboost = pboost.E()*pboost.E() - pboost.X()*pboost.X() - pboost.Y()*pboost.Y() - pboost.Z()*pboost.Z();
+  if (rmboost>0.0){
+    rmboost = TMath::Sqrt(rmboost);
+  }else{
+    rmboost = 0.0;
+  }
+  
+  double aux = (q.E()*pboost.E() - q.X()*pboost.X() - q.Y()*pboost.Y() - q.Z()*pboost.Z()) / rmboost ;
+  double aaux = (aux+q.E()) / (pboost.E()+rmboost);
+  
+  double qprimeE = aux;
+  double qprimeX = q.X() - aaux*pboost.X();
+  double qprimeY = q.Y() - aaux*pboost.Y();
+  double qprimeZ = q.Z() - aaux*pboost.Z();
+
+  qprime.SetPxPyPzE(qprimeX, qprimeY, qprimeZ, qprimeE);
+  
+  return qprime;
+}
+
+
+Double_t distTheta00(Double_t costheta, Double_t costheta1){
+
+  Double_t distTheta00_ = 16.0*(cR*cR*cR*cR + 2*cL*cL*cR*cR + cL*cL*cL*cL)*(1.0 - costheta*costheta)*(1.0 - costheta1*costheta1);
+  return distTheta00_;  
+}
+
+Double_t distThetaLL(Double_t costheta, Double_t costheta1){
+
+  Double_t distThetaLL_ = 4.0*(cR*cR*cR*cR*(1.0 + costheta)*(1.0 + costheta)*(1.0 + costheta1)*(1.0 + costheta1) + cL*cL*cR*cR*((1.0 + costheta)*(1.0 + costheta)*(1.0 - costheta1)*(1.0 - costheta1) + (1.0 - costheta)*(1.0 - costheta)*(1.0 + costheta1)*(1.0 + costheta1)) + cL*cL*cL*cL*(1.0 - costheta)*(1.0 - costheta)*(1.0 - costheta1)*(1.0 - costheta1));
+  return distThetaLL_;
+}
+
+Double_t distThetaRR(Double_t costheta, Double_t costheta1){
+
+  Double_t distThetaRR_ = 4.0*(cR*cR*cR*cR*(1.0 - costheta)*(1.0 - costheta)*(1.0 - costheta1)*(1.0 - costheta1) + cL*cL*cR*cR*((1.0 + costheta)*(1.0 + costheta)*(1.0 - costheta1)*(1.0 - costheta1) + (1.0 - costheta)*(1.0 - costheta)*(1.0 + costheta1)*(1.0 + costheta1)) + cL*cL*cL*cL*(1.0 + costheta)*(1.0 + costheta)*(1.0 + costheta1)*(1.0 + costheta1));
+  return distThetaRR_;
+}
+
+Double_t distTheta0L(Double_t costheta, Double_t costheta1, Double_t cosphi){
+
+  Double_t sintheta = TMath::Sqrt(1.0-costheta*costheta);
+  Double_t sintheta1 = TMath::Sqrt(1.0-costheta1*costheta1);
+
+  Double_t disTheta0L_ = -16.0*( cR*cR*cR*cR*(1.0 + costheta)*(1.0 + costheta1) + cL*cL*cL*cL*(1.0 - costheta)*(1.0 - costheta1) - cL*cL*cR*cR*( (1.0 + costheta)*(1.0 - costheta1) + (1.0-costheta)*(1.0+costheta1)))*sintheta*sintheta1*cosphi;
+
+  return disTheta0L_;
+}
+
+Double_t distTheta0R(Double_t costheta, Double_t costheta1, Double_t cosphi){
+
+  Double_t sintheta = TMath::Sqrt(1.0-costheta*costheta);
+  Double_t sintheta1 = TMath::Sqrt(1.0-costheta1*costheta1);
+
+  Double_t disTheta0R_ =-16.0*( cR*cR*cR*cR*(1.0 - costheta)*(1.0 - costheta1) + cL*cL*cL*cL*(1.0 + costheta)*(1.0 + costheta1) - cL*cL*cR*cR*( (1.0 + costheta)*(1.0 - costheta1) + (1.0-costheta)*(1.0+costheta1)))*sintheta*sintheta1*cosphi;
+
+  return disTheta0R_;
+}
+
+Double_t distThetaLR(Double_t costheta, Double_t costheta1, Double_t cosphi){
+
+  Double_t sintheta = TMath::Sqrt(1.0-costheta*costheta);
+  Double_t sintheta1 = TMath::Sqrt(1.0-costheta1*costheta1);
+  Double_t cos2phi = 2.0*cosphi*cosphi -1.0;
+
+  Double_t disThetaLR_ = 8.0*(cL*cL + cR*cR)*(cL*cL + cR*cR)*sintheta*sintheta*sintheta1*sintheta1*cos2phi;
+  return disThetaLR_;
+}
+
+
 /**
 ######################################################
 ##                                                  ##
@@ -54,6 +184,17 @@ public:
   unsigned getNdata() override {return 1; }
   //This function will return the required value
   double evaluate(unsigned) override;
+
+  Double_t MW = 80.3579736098775;
+  Double_t WW = 2.084298998278219;
+  Double_t s2w = 0.22283820939806098;
+  Double_t c2w = 0.777161790601939;
+  Double_t cL= 4.0*sqrt(c2w/2.0);
+  Double_t cR = 0.0;
+
+  Double_t GF = ROOT::Math::sqrt(2)/(2*246*246);
+  Double_t gW = ROOT::Math::sqrt((GF/ROOT::Math::sqrt(2)) * 8 * MW*MW);
+  Double_t ghWW = gW*MW;
 
 protected:
   void bindTree_(multidraw::FunctionLibrary&) override;
@@ -101,18 +242,7 @@ DoHiggsPolarizationWeight::evaluate(unsigned)
   TLorentzVector WM;
   TLorentzVector H;
   
-  Double_t MW = 8.041900e+01;
-  Double_t WW = 2.047600e+00;
-  Double_t ghWW = 2*MW*MW/246;
-  Double_t GF = ROOT::Math::sqrt(2)/(2*246*246);
-  
-  //Double_t cL = GF*MW*MW*MW/(6.0 * ROOT::Math::Pi()*ROOT::Math::sqrt(2));
-  //Double_t cR = GF*MW*MW*MW/(6.0 * ROOT::Math::Pi()*ROOT::Math::sqrt(2));
- 
-  Double_t cL = 0.0;
-  Double_t cR = GF*MW*MW*MW/(6.0 * ROOT::Math::Pi()*ROOT::Math::sqrt(2));
- 
-    
+       
   Int_t number_elec = 0;
   Int_t number_muon = 0;
   Int_t number_tau = 0;
@@ -125,7 +255,7 @@ DoHiggsPolarizationWeight::evaluate(unsigned)
   unsigned int nGen = GenPart_pt->GetSize();
     
   for (unsigned int p = 0; p < nGen; p++){
-  
+
     // Leptons
     
     mother_pos = GenPart_genPartIdxMother->At(p);
@@ -240,84 +370,209 @@ DoHiggsPolarizationWeight::evaluate(unsigned)
   // Boost over lepton from the Ws reference frame
   // Compute theta star  
 
-  WP = vector_Wp;
-  WM = vector_Wm;
+  WP = vector_lp + vector_nup;
+  WM = vector_lm + vector_num;
   H = WP + WM;
-  
-  genH.SetCoordinates(H.Pt(), H.Eta(), H.Phi(), H.E());
-  
-  ROOT::Math::XYZVector hRF;  
-  hRF = genH.BoostToCM();
-  
-  ROOT::Math::XYZVector leppWRF;
-  ROOT::Math::XYZVector nupWRF;
-  
-  ROOT::Math::XYZVector WpHRF;
-  ROOT::Math::XYZVector WmHRF;
 
-  WpHRF = ROOT::Math::VectorUtil::boost(genWp, hRF);
-  WmHRF = ROOT::Math::VectorUtil::boost(genWm, hRF);
-  
-  leppWRF = ROOT::Math::VectorUtil::boost(genlp, WpHRF);
-  nupWRF = ROOT::Math::VectorUtil::boost(gennum, WmHRF);
-  
-  Double_t theta_Wp_star = ROOT::Math::VectorUtil::Angle(leppWRF, genWp);
-  Double_t theta_Wm_star = ROOT::Math::VectorUtil::Angle(nupWRF, genWm);
+  // Leptons in the Lab frame
+  TLorentzVector L11 = vector_lp;
+  TLorentzVector L12 = vector_nup;
+  TLorentzVector L21 = vector_num;
+  TLorentzVector L22 = vector_lm;
 
-  Double_t cos_Wp_theta_star = ROOT::Math::cos(theta_Wp_star);
-  Double_t cos_Wm_theta_star = ROOT::Math::cos(theta_Wm_star);
-  Double_t dphill = leppWRF.Phi() - nupWRF.Phi();
+  /**
+  // Leptons in the 4l frame (Higgs)
+  TLorentzVector L114l = vector_lp;
+  TLorentzVector L124l = vector_nup;
+  TLorentzVector L214l = vector_num;
+  TLorentzVector L224l = vector_lm;
 
+  // Bosons in the 4l frame (Higgs)
+  TLorentzVector WP4l = WP;
+  TLorentzVector WM4l = WM;
+  
+  L114l.Boost(-H.BoostVector());
+  L124l.Boost(-H.BoostVector());
+  L214l.Boost(-H.BoostVector());
+  L224l.Boost(-H.BoostVector());
+
+  WP4l.Boost(-H.BoostVector());
+  WM4l.Boost(-H.BoostVector());
+  **/
+
+  TLorentzVector L114l = boostinv(L11, H);
+  TLorentzVector L124l = boostinv(L12, H);
+  TLorentzVector L214l = boostinv(L21, H);
+  TLorentzVector L224l = boostinv(L22, H);
+
+  TLorentzVector WP4l = boostinv(WP, H);
+  TLorentzVector WM4l = boostinv(WM, H);
+
+
+  // Leptons in the V(W/Z) frame                                                                                                                                                                                                             
+  /**
+  TLorentzVector L11V = L114l;
+  TLorentzVector L12V = L124l;
+  TLorentzVector L21V = L214l;
+  TLorentzVector L22V = L224l;
+
+  L11V.Boost(-WP4l.BoostVector());
+  L21V.Boost(-WM4l.BoostVector());
+  **/
+
+  TLorentzVector L11V = boostinv(L114l, WP4l);
+  TLorentzVector L21V = boostinv(L214l, WM4l);
+
+  
+  Double_t rmodwp = TMath::Sqrt(WP4l.X()*WP4l.X() + WP4l.Y()*WP4l.Y() + WP4l.Z()*WP4l.Z());
+  Double_t rmodwm = TMath::Sqrt(WM4l.X()*WM4l.X() + WM4l.Y()*WM4l.Y() + WM4l.Z()*WM4l.Z());
+  Double_t rmod11 = TMath::Sqrt(L11V.X()*L11V.X() + L11V.Y()*L11V.Y() + L11V.Z()*L11V.Z());
+  Double_t rmod21 = TMath::Sqrt(L21V.X()*L21V.X() + L21V.Y()*L21V.Y() + L21V.Z()*L21V.Z());
+
+  Double_t sprodwp = WP4l.X()*L11V.X() + WP4l.Y()*L11V.Y() + WP4l.Z()*L11V.Z();
+  Double_t sprodwm = WM4l.X()*L21V.X() + WM4l.Y()*L21V.Y() + WM4l.Z()*L21V.Z();
+
+  Double_t costhetawp = sprodwp / (rmodwp*rmod11);
+  Double_t costhetawm = sprodwm / (rmodwm*rmod21);
+
+  TVector3 p3wp = WP4l.Vect();
+  TVector3 p3wm = WM4l.Vect();
+  TVector3 p3l11 = L114l.Vect();
+  TVector3 p3l21 = L214l.Vect();
+
+  // Rotation
+  //TVector3 p3l11R = p3l11;
+  //TVector3 p3l21R = p3l21;
+
+  //p3l11R.RotateUz(p3wp);
+  //p3l21R.RotateUz(p3wp);
+
+  TVector3 p3l11R = rotinv(p3l11, p3wp);
+  TVector3 p3l21R = rotinv(p3l21, p3wp);
+  
+  /**
+  Double_t qmodt = p3wp.X()*p3wp.X() + p3wp.Y()*p3wp.Y();
+  Double_t qmod = qmodt + p3wp.Z()*p3wp.Z();
+  qmodt = TMath::Sqrt(qmodt);
+  qmod = TMath::Sqrt(qmod);
+
+  Double_t cth = p3wp.Z()/qmod;
+  Double_t sth = 1.0 - cth*cth;
+
+  if (sth==0.0 || qmodt==0.0){
+    p3l11 = L114l.Vect();
+    p3l21 = L214l.Vect();
+  }else{
+
+    sth = TMath::Sqrt(sth);
+    Double_t cfi = p3wp.X()/qmodt;
+    Double_t sfi = p3wp.Y()/qmodt;
+
+    p3l11R.SetXYZ(cth*cfi*p3l11.X()+cth*sfi*p3l11.Y()-sth*p3l11.Z(), -sfi*p3l11.X()+cfi*p3l11.Y(), sth*cfi*p3l11.X()+sth*sfi*p3l11.Y()+cth*p3l11.Z());
+    p3l21R.SetXYZ(cth*cfi*p3l21.X()+cth*sfi*p3l21.Y()-sth*p3l21.Z(), -sfi*p3l21.X()+cfi*p3l21.Y(), sth*cfi*p3l21.X()+sth*sfi*p3l21.Y()+cth*p3l21.Z());
+  }
+  **/
+
+
+  Double_t pt11 = TMath::Sqrt(p3l11R.X()*p3l11R.X() + p3l11R.Y()*p3l11R.Y());
+  Double_t pt21 = TMath::Sqrt(p3l21R.X()*p3l21R.X() + p3l21R.Y()*p3l21R.Y());
+  
+  
+  Double_t cosphi = (p3l11R.X()*p3l21R.X() + p3l11R.Y()*p3l21R.Y()) / (pt11*pt21);
+  Double_t dphill = TMath::ACos(cosphi);
+  
+
+  /**
   // https://arxiv.org/pdf/2105.07972.pdf
   
   /////////////////////////////////////
   //             |ALL|^2             //
   /////////////////////////////////////
+
   
   Double_t K = ((H.M()*H.M() - WP.M()*WP.M() - WM.M()*WM.M()) / (2 * WP.M() * WM.M()));
-  Double_t P1 = ( (2 * ghWW * WP.M() * WP.M()) / ((WP.M() * WP.M() - MW*MW)*(WP.M() * WP.M() - MW * MW) + WW * WW * MW * MW));
-  Double_t P2 = ( (2 * ghWW * WM.M() * WM.M()) / ((WM.M() * WM.M() - MW*MW)*(WM.M() * WM.M() - MW * MW) + WW * WW * MW * MW));
-  Double_t ALL2 = 4 * K*K * P1 * P2 * (cL*cL + cR*cR)*(cL*cL + cR*cR) * ROOT::Math::sin(theta_Wp_star)*ROOT::Math::sin(theta_Wp_star) * ROOT::Math::sin(theta_Wm_star)*ROOT::Math::sin(theta_Wm_star);
-  
+
+
+  Double_t ALL2 = K*K * 16*(cR*cR*cR*cR + 2*(cL*cL)*(cR*cR) + cL*cL*cL*cL)*(1.0-costhetawp*costhetawp)*(1.0-costhetawm*costhetawm);
+
   /////////////////////////////////////
   //             |A++|^2             //
   /////////////////////////////////////
+
+  // Z Decay
+  //Double_t plus1 = cL*cL*cL*cL * (1 + ROOT::Math::cos(theta_Wp_star))*(1 + ROOT::Math::cos(theta_Wp_star)) * (1 + ROOT::Math::cos(theta_Wm_star))*(1 + ROOT::Math::cos(theta_Wm_star));
+  //Double_t plus2 = cR*cR*cR*cR * (1 - ROOT::Math::cos(theta_Wp_star))*(1 - ROOT::Math::cos(theta_Wp_star)) * (1 - ROOT::Math::cos(theta_Wm_star))*(1 - ROOT::Math::cos(theta_Wm_star));
+  //Double_t plus3 = cR*cR*cL*cL * (1 + ROOT::Math::cos(theta_Wp_star))*(1 + ROOT::Math::cos(theta_Wp_star)) * (1 - ROOT::Math::cos(theta_Wm_star))*(1 - ROOT::Math::cos(theta_Wm_star));
+  //Double_t plus4 = cR*cR*cL*cL * (1 - ROOT::Math::cos(theta_Wp_star))*(1 - ROOT::Math::cos(theta_Wp_star)) * (1 + ROOT::Math::cos(theta_Wm_star))*(1 + ROOT::Math::cos(theta_Wm_star));
+  //
+  //Double_t App2 = P1 * P2 * (plus1 + plus2 + plus3 + plus4);
   
-  Double_t plus1 = cL*cL*cL*cL * (1 + ROOT::Math::cos(theta_Wp_star))*(1 + ROOT::Math::cos(theta_Wp_star)) * (1 + ROOT::Math::cos(theta_Wm_star))*(1 + ROOT::Math::cos(theta_Wm_star));
-  Double_t plus2 = cR*cR*cR*cR * (1 - ROOT::Math::cos(theta_Wp_star))*(1 - ROOT::Math::cos(theta_Wp_star)) * (1 - ROOT::Math::cos(theta_Wm_star))*(1 - ROOT::Math::cos(theta_Wm_star));
-  Double_t plus3 = cR*cR*cL*cL * (1 + ROOT::Math::cos(theta_Wp_star))*(1 + ROOT::Math::cos(theta_Wp_star)) * (1 - ROOT::Math::cos(theta_Wm_star))*(1 - ROOT::Math::cos(theta_Wm_star));
-  Double_t plus4 = cR*cR*cL*cL * (1 - ROOT::Math::cos(theta_Wp_star))*(1 - ROOT::Math::cos(theta_Wp_star)) * (1 + ROOT::Math::cos(theta_Wm_star))*(1 + ROOT::Math::cos(theta_Wm_star));
-  
-  Double_t App2 = P1 * P2 * (plus1 + plus2 + plus3 + plus4);
-  
+  // W Decay
+  Double_t plus1 = 4*cR*cR*cR*cR * (1 + costhetawp) * (1 + costhetawp) * (1 + costhetawm) * (1 + costhetawm);
+  Double_t plus2 = 4*cL*cL*cL*cL * (1 - costhetawp) * (1 - costhetawp) * (1 - costhetawm) * (1 - costhetawm);
+  Double_t plus3 = 4*cL*cL*cR*cR * (1 + costhetawp) * (1 + costhetawp) * (1 - costhetawm) * (1 - costhetawm);
+  Double_t plus4 = 4*cR*cR*cL*cL * (1 - costhetawp) * (1 - costhetawp) * (1 + costhetawm) * (1 + costhetawm);
+
+  Double_t App2 = plus1 + plus2 + plus3 + plus4;
+
+
   /////////////////////////////////////
   //             |A--|^2             //
   /////////////////////////////////////
   
-  Double_t minus1 = cL*cL*cL*cL * (1 - ROOT::Math::cos(theta_Wp_star))*(1 - ROOT::Math::cos(theta_Wp_star)) * (1 - ROOT::Math::cos(theta_Wm_star))*(1 - ROOT::Math::cos(theta_Wm_star));
-  Double_t minus2 = cR*cR*cR*cR * (1 + ROOT::Math::cos(theta_Wp_star))*(1 + ROOT::Math::cos(theta_Wp_star)) * (1 + ROOT::Math::cos(theta_Wm_star))*(1 + ROOT::Math::cos(theta_Wm_star));
-  Double_t minus3 = cR*cR*cL*cL * (1 + ROOT::Math::cos(theta_Wp_star))*(1 + ROOT::Math::cos(theta_Wp_star)) * (1 - ROOT::Math::cos(theta_Wm_star))*(1 - ROOT::Math::cos(theta_Wm_star));
-  Double_t minus4 = cR*cR*cL*cL * (1 - ROOT::Math::cos(theta_Wp_star))*(1 - ROOT::Math::cos(theta_Wp_star)) * (1 + ROOT::Math::cos(theta_Wm_star))*(1 + ROOT::Math::cos(theta_Wm_star));
+
+  // Z Decay
+  //Double_t minus1 = cL*cL*cL*cL * (1 - ROOT::Math::cos(theta_Wp_star))*(1 - ROOT::Math::cos(theta_Wp_star)) * (1 - ROOT::Math::cos(theta_Wm_star))*(1 - ROOT::Math::cos(theta_Wm_star));
+  //Double_t minus2 = cR*cR*cR*cR * (1 + ROOT::Math::cos(theta_Wp_star))*(1 + ROOT::Math::cos(theta_Wp_star)) * (1 + ROOT::Math::cos(theta_Wm_star))*(1 + ROOT::Math::cos(theta_Wm_star));
+  //Double_t minus3 = cR*cR*cL*cL * (1 + ROOT::Math::cos(theta_Wp_star))*(1 + ROOT::Math::cos(theta_Wp_star)) * (1 - ROOT::Math::cos(theta_Wm_star))*(1 - ROOT::Math::cos(theta_Wm_star));
+  //Double_t minus4 = cR*cR*cL*cL * (1 - ROOT::Math::cos(theta_Wp_star))*(1 - ROOT::Math::cos(theta_Wp_star)) * (1 + ROOT::Math::cos(theta_Wm_star))*(1 + ROOT::Math::cos(theta_Wm_star));
   
-  Double_t Amm2 = P1 * P2 * (minus1 + minus2 + minus3 + minus4);
+  //Double_t Amm2 = P1 * P2 * (minus1 + minus2 + minus3 + minus4);
+
+  // W Decay
+  Double_t minus1 = 4*cR*cR*cR*cR * (1 - costhetawp) * (1 - costhetawp) * (1 - costhetawm) * (1 - costhetawm);
+  Double_t minus2 = 4*cL*cL*cL*cL * (1 + costhetawp) * (1 + costhetawp) * (1 + costhetawm) * (1 + costhetawm);
+  Double_t minus3 = 4*cL*cL*cR*cR * (1 - costhetawp) * (1 - costhetawp) * (1 + costhetawm) * (1 + costhetawm);
+  Double_t minus4 = 4*cR*cR*cL*cL * (1 + costhetawp) * (1 + costhetawp) * (1 - costhetawm) * (1 - costhetawm);
+
+  Double_t Amm2 = minus1+ minus2+ minus3+ minus4;
   
+
   /////////////////////////////////////
   //            2Re(ALL++)           //
   /////////////////////////////////////
-  
-  Double_t ReALLpp = -4 * K * P1 * P2 * (plus1 + plus2 - plus3 - plus4) * ROOT::Math::sin(theta_Wp_star) * ROOT::Math::sin(theta_Wm_star) * ROOT::Math::cos(dphill);
-  
+
+  Double_t sinthetawp = TMath::Sqrt(1.0-costhetawp*costhetawp);
+  Double_t sinthetawm = TMath::Sqrt(1.0-costhetawm*costhetawm);
+
+
+  Double_t LLpp1 = cL*cL*cL*cL * (1 - costhetawp) * (1 - costhetawm);
+  Double_t LLpp2 = cR*cR*cR*cR * (1 + costhetawp) * (1 + costhetawm);
+  Double_t LLpp3 = cL*cL*cR*cR * (1 + costhetawp) * (1 - costhetawm);
+  Double_t LLpp4 = cR*cR*cL*cL * (1 - costhetawp) * (1 + costhetawm);
+
+  Double_t ReALLpp = -16.0 * K * (LLpp1+LLpp2+LLpp3+LLpp4) * sinthetawp * sinthetawm * cosphi;
+
+
   /////////////////////////////////////
   //            2Re(ALL--)           //
   /////////////////////////////////////
-  
-  Double_t ReALLmm = -4 * K * P1 * P2 * (minus1 + minus2 - minus3 - minus4) * ROOT::Math::sin(theta_Wp_star) * ROOT::Math::sin(theta_Wm_star) * ROOT::Math::cos(dphill);
+
+  Double_t LLmm1 = cL*cL*cL*cL * (1 + costhetawp) * (1 + costhetawm);
+  Double_t LLmm2 = cR*cR*cR*cR * (1 - costhetawp) * (1 - costhetawm);
+  Double_t LLmm3 = cL*cL*cR*cR * (1 - costhetawp) * (1 + costhetawm);
+  Double_t LLmm4 = cR*cR*cL*cL * (1 + costhetawp) * (1 - costhetawm);
+
+  Double_t ReALLmm = -16.0 * K * (LLmm1+LLmm2+LLmm3+LLmm4) * sinthetawp * sinthetawm * cosphi;
+
 
   /////////////////////////////////////
   //            2Re(A++--)           //
   /////////////////////////////////////
-  
-  Double_t ReAppmm = 2 * P1 * P2 * (cL*cL + cR*cR)*(cL*cL + cR*cR) * ROOT::Math::sin(theta_Wp_star)*ROOT::Math::sin(theta_Wp_star) * ROOT::Math::sin(theta_Wm_star)*ROOT::Math::sin(theta_Wm_star) * ROOT::Math::cos(2*dphill);
+
+  Double_t cos2phi = 2.0*cosphi*cosphi - 1.0;
+
+  Double_t ReAppmm = 8.0 * ((cL*cL + cR*cR)*(cL*cL + cR*cR)) * sinthetawp*sinthetawp * sinthetawm*sinthetawm * cos2phi;
   
   
   /////////////////////////////////////
@@ -325,18 +580,48 @@ DoHiggsPolarizationWeight::evaluate(unsigned)
   /////////////////////////////////////
   
   Double_t ATT2 = App2 + Amm2 + ReAppmm;
-  
-  
-  // Final weights
-  
+  **/
+
+
+
+  Double_t Q1 = H.E()*H.E() - H.X()*H.X() - H.Y()*H.Y() - H.Z()*H.Z();
+  Double_t Q2 = WP.E()*WP.E() - WP.X()*WP.X() - WP.Y()*WP.Y() - WP.Z()*WP.Z();
+  Double_t Q3 = WM.E()*WM.E() - WM.X()*WM.X() - WM.Y()*WM.Y() - WM.Z()*WM.Z();
+  Double_t epsfact = (Q1-Q2-Q3)/TMath::Sqrt(Q2*Q3)*0.5;
+
+  Double_t K = epsfact;
+
+  Double_t ALL2 = epsfact*epsfact * distTheta00(costhetawp, costhetawm);
+  Double_t App2 = distThetaLL(costhetawp, costhetawm);
+  Double_t Amm2 = distThetaRR(costhetawp, costhetawm);
+  Double_t ReALLpp = epsfact*distTheta0L(costhetawp, costhetawm, cosphi);
+  Double_t ReALLmm = epsfact*distTheta0R(costhetawp, costhetawm, cosphi);
+  Double_t ReAppmm = distThetaLR(costhetawp, costhetawm, cosphi);
+
+  Double_t ATT2 = App2 + Amm2 + ReAppmm;
+
+  // Final weights  
   Double_t weight_LL = ALL2 / (ALL2 + ATT2 + ReALLpp + ReALLmm);
   
   Double_t weight_TT = ATT2 / (ALL2 + ATT2 + ReALLpp + ReALLmm);
+
+  Double_t weight_Int = (ReALLpp + ReALLmm) / (ALL2 + ATT2 + ReALLpp + ReALLmm);
   
+
   if (name_=="LL"){
     return (double)weight_LL;
   }else if(name_=="TT"){
     return (double)weight_TT;
+  }else if(name_=="Int"){
+    return (double)weight_Int;
+  }else if (name_=="num"){
+    return (double)ALL2;
+  }else if (name_=="den"){
+    return (double)(ALL2 + ATT2 + ReALLpp + ReALLmm);
+  }else if (name_=="phi"){
+    return dphill;
+  }else if(name_=="theta1"){
+    return costhetawp;
   }else{
     return -9999.9;
   }
